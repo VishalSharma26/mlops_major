9a89c5b8ed6da76a80395240f65713b44cd0ef45
Bharat Vishal Sharma <bharatvishal26@gmail.com>
2025-07-30 14:51:29 +0000

    Added Dockerfile and predict script

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Use Python image
+FROM python:3.10-slim
+
+# Set working directory
+WORKDIR /app
+
+# Copy files
+COPY requirements.txt requirements.txt
+COPY src/ src/
+COPY tests/ tests/
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Run training by default
+CMD ["python", "src/train.py"]
---
+++ src/predict.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import joblib
+from utils import load_data
+
+BASE_DIR   = os.path.join(os.path.dirname(__file__), "../")
+MODEL_FILE = os.path.join(BASE_DIR, "model", "linear_model.joblib")
+
+if not os.path.exists(os.path.dirname(MODEL_FILE)):
+    os.makedirs(os.path.dirname(MODEL_FILE))
+
+def load():
+    return joblib.load(MODEL_FILE)
+
+def predict():
+    # Load data
+    _, X_test, _, y_test = load_data()
+
+    # Load model
+    model = load()
+
+    # Predict
+    predictions = model.predict(X_test[:5])
+    print("Sample Predictions:", predictions)
+    print("Actual Values:", y_test[:5])
+
+if __name__ == "__main__":
+    predict()
